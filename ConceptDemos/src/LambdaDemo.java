06a8d84295530a012e3951422c4ccc79c7dc0864
GrimWasTaken <54084237+GrimWasTaken@users.noreply.github.com>
2019-10-20 03:44:59 -0400

    Initial commit

---
+++ ConceptDemos/src/LambdaDemo.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+interface Operate{
+	public int add(int whatToAdd);
+}
+
+interface Filler{
+	public String fill();
+}
+
+interface Supplier<Type> {
+    Type get();
+}
+
+public class LambdaDemo{
+	public static void main(String[] args) {
+		
+				//Lambda section
+				//Define single abstract methods
+				Operate doMath = a -> a += 5;
+				Filler baster = () -> "Jelly Donut";
+				
+				//Define variables that implement said methods
+				String food = baster.fill();
+				int num = doMath.add(5);
+				
+				//Display results
+				System.out.println(num);
+				System.out.println(food);
+
+				
+				//Method reference section
+				Supplier<String> response = baster :: fill;
+				
+				//Display results
+				System.out.println(response.get());
+				
+				
+				//Stream section
+		        Stream<String> burger = Stream.of("Cheese", "Burger"); 
+		        
+		        burger.forEach(b -> System.out.print(b)); 
+	}
+}
